63946c117055dfff2ce4e8ff9bf8646111e6f061
Kanwaljeet Singh <kawal20905@gmail.com>
2025-05-21 11:04:23 +0530

    Delete Session2 directory

--- Session2/p2.py
+++
@@ -1,7 +0,0 @@
-angle1=int(input("Enter 1st angle - "))
-angle2=int(input("Enter 2nd angle - "))
-angle3=int(input("Enter 3rd angle - "))
-if(angle1+angle2+angle3)==180:
-    print("Can form a triangle")
-else:
-    print("Cannot form a triangle")    
